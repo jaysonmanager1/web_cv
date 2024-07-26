c7e1bddfee413119751faf78d3a8b950e86b52fa
jayson1606 <nguyenhoangson16062004@gmail.com>
2024-07-26 15:43:07 +0700

    fix_send_to_every_email

--- js/handle_email_send.js
+++ js/handle_email_send.js
@@ -1,9 +1,6 @@
-(function () {
-    emailjs.init("Y97n60iQtoJL5MCrc");
-})();
-
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
+
     document.getElementById('colorlib-aside').style.zIndex = -1;
     // Hiển thị thông báo loading
     document.getElementById('loading').style.display = 'block';
@@ -16,27 +13,25 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
         alert('Vui lòng nhập địa chỉ email hợp lệ.');
+        document.getElementById('loading').style.display = 'none'; // Ẩn thông báo loading
         return; // Dừng việc gửi email nếu địa chỉ email không hợp lệ
     }
+
     else {
 
-        // Nếu địa chỉ email hợp lệ, tiếp tục gửi email
-        var params = {
-            name: document.getElementById('name').value,
-            email: email,
-            subject: document.getElementById('subject').value,
-            message: document.getElementById('message').value
-        };
+        const serviceID = 'default_service';
+        const templateID = 'template_h3o3b72';
 
-        emailjs.send('service_rhuo8bo', 'template_h3o3b72', params)
+        emailjs.sendForm(serviceID, templateID, this)
             .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                alert('Email đã được gửi thành công!');
-
-                document.getElementById('loading').style.display = 'none'; // Ẩn thông báo loading
                 setTimeout(function () {
                     location.reload(); // Reload lại trang sau khi gửi email thành công
                 }, 1000); // Thời gian trễ trước khi reload (1000ms = 1 giây)
+                document.getElementById('loading').style.display = 'none'; // Ẩn thông báo loading
+
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Email đã được gửi thành công!');
+
 
             }, function (error) {
                 console.log('THẤT BẠI...', error);
