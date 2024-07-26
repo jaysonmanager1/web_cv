328df8e858e6ee69b7fd3bcdd86e90a758fb303d
jayson1606 <nguyenhoangson16062004@gmail.com>
2024-07-26 13:21:21 +0700

    update_fix_email_send_to

--- js/handle_email_send.js
+++ js/handle_email_send.js
@@ -4,10 +4,14 @@
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
+    document.getElementById('colorlib-aside').style.zIndex = -1;
+    // Hiển thị thông báo loading
+    document.getElementById('loading').style.display = 'block';
 
     // Lấy giá trị email từ input
     var email = document.getElementById('email').value;
     console.log(email);
+
     // Kiểm tra định dạng email bằng biểu thức chính quy
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
@@ -28,9 +32,16 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 alert('Email đã được gửi thành công!');
+
+                document.getElementById('loading').style.display = 'none'; // Ẩn thông báo loading
+                setTimeout(function () {
+                    location.reload(); // Reload lại trang sau khi gửi email thành công
+                }, 1000); // Thời gian trễ trước khi reload (1000ms = 1 giây)
+
             }, function (error) {
                 console.log('THẤT BẠI...', error);
                 alert('Không thể gửi email. Vui lòng thử lại sau.');
+                document.getElementById('loading').style.display = 'none'; // Ẩn thông báo loading
             });
     }
 
